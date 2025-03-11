c3f0674321d3c342497cc15f028564915a739efa
Михаил Васильев <mgp-nsk@yandex.ru>
2025-03-11 17:00:33 +0700

    react-hook-form installed

--- src/routes/SleepingRoute/SleepingRoute.js
+++ src/routes/SleepingRoute/SleepingRoute.js
@@ -2,36 +2,56 @@ import React from "react";
 import "./SleepingRoute.css";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 const SleepingRoute = ({ onSetSleeping, className }) => {
+  const { register, handleSubmit, control } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className={`sleeping-route${className ? " " + className : ""}`}>
       <h2>Сон</h2>
-      <form className="mb-3">
+      <form className="mb-3" onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <Button type="button">Лечь спать</Button>
-          <Button type="button">Проснуться</Button>
-          <Button type="button">Ввести вручную</Button>
+          <Button>Лечь спать</Button>
+          <Button>Проснуться</Button>
+          <Button>Ввести вручную</Button>
         </div>
         <div className="d-flex">
           <Input
             label="Начало"
             id="sleepingStartDatetime"
             type="datetime-local"
+            {...register("sleepingStartDatetime")}
           />
           <Input
             label="Окончание"
             id="sleepingFinishDatetime"
             type="datetime-local"
+            {...register("sleepingFinishDatetime")}
           />
+          {/* <input
+            label="Начало"
+            id="sleepingStartDatetime"
+            type="datetime-local"
+            {...register("sleepingStartDatetime")}
+          />
+          <input
+            label="Окончание"
+            id="sleepingFinishDatetime"
+            type="datetime-local"
+            {...register("sleepingFinishDatetime")}
+          /> */}
+        </div>
+        <div>
+          <Button>Сбросить</Button>
+          <Button type="submit" onClick={onSetSleeping}>
+            Сохранить
+          </Button>
         </div>
       </form>
-      <div>
-        <Button type="button">Сбросить</Button>
-        <Button type="button" onClick={onSetSleeping}>
-          Сохранить
-        </Button>
-      </div>
+      <DevTool control={control} />
     </div>
   );
 };
